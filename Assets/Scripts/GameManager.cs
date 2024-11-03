ead390e24fff9ca08bbd4d7611c0e6f1715d4272
Theo Morris <pandazflipperpanda706@gmail.com>
2024-11-03 17:15:13 +1300

    Gamemanger rework

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,6 +8,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
     [HideInInspector] public GameState GameState;
     private double m_CurrentMoney;
+    public string mainMenuScene = "MainMenu";
 
     public double CurrentMoney
     {
@@ -36,5 +38,31 @@ public class GameManager : MonoBehaviour
         }
 
         //UnSerializeGameStateFromJson();
+    }
+
+    public IEnumerator Init()
+    {
+        //load from save files
+
+
+        yield return null;
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Loading Game Scene... ");
+        MainMenuManager.Instance.OpenLoadingPage();
+        SceneManager.LoadSceneAsync("GameScene");
+    }
+
+    public void LoadMainMenu()
+    {
+        Debug.Log("Loading Main Menu... ");
+        SceneManager.LoadScene(mainMenuScene);
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
 }
